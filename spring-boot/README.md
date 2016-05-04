4d1bca05818b6d8fe1f0b89947801954d19922cd
Prokop, Bart <bprokop@cybersource.com>
2016-05-04 11:49:22 +0100

    adding Spring Boot entry point

--- spring-boot/README.md
+++ spring-boot/README.md
@@ -5,3 +5,6 @@ A mocked merchant checkout and payment pages using Flex-API tokenization.
 To run this demo modify ./src/main/resources/application.properties and invoke:
 
     mvn clean spring-boot:run
+
+Note: to run this example you *must* install the unlimited policy files in the JVM you are using.
+These (for Java 8) can be downloaded from http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
---
+++ spring-boot/src/main/java/com/cybersource/flex/samples/EntryPoint.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2016 CyberSource Corporation. All rights reserved.
+ */
+package com.cybersource.flex.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryPoint {
+
+    public static void main(String... args) throws Exception {
+        SpringApplication.run(EntryPoint.class, args);
+    }
+
+}
