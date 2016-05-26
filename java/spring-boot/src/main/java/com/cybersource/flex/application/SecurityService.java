be3ce9be88fd5549bcae46fb36dbd5f1c218879a
Donaldson, James <jadonald@visa.com>
2016-05-26 17:48:48 +0100

    Adds a simplified java example

---
+++ java/spring-boot/src/main/java/com/cybersource/flex/application/SecurityService.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2016 CyberSource Corporation. All rights reserved.
+ */
+package com.cybersource.flex.application;
+
+import java.security.KeyFactory;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.Signature;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService {
+
+    private static final Base64.Encoder ENCODER = Base64.getEncoder();
+    private static final Base64.Decoder DECODER = Base64.getDecoder();
+
+    @Value("${cmmKey}")
+    private String cmmKey;
+    @Value("${organizationId}")
+    private String organizationId;
+    @Value("${keyStoreFile}")
+    private String keyStoreFile;
+    @Value("${keyStorePassword}")
+    private String keyStorePassword;
+    @Value("${privateKeyPassword}")
+    private String privateKeyPassword;
+
+    public SecurityService() {
+        // Add Bouncy Castle JCE provider dynamically.
+        // http://www.bouncycastle.org/wiki/display/JA1/Provider+Installation
+        // Note: to run this example you *must* install the unlimited policy files in the JVM you are using.
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    /**
+     * This method adds HTTP authentication headers.
+     * 
+     * @param headers map to be added to
+     */
+    public void addSignature(HttpHeaders headers) {
+        try {
+            // data used to generate signature
+            final long time = System.currentTimeMillis();
+            final String contentType = "application/json";
+            final String httpVerb = "post";
+
+            // Retrieving RSA private key from P12 keystore
+            KeyStore keyStore = KeyStore.getInstance("PKCS12", BouncyCastleProvider.PROVIDER_NAME);
+            ClassPathResource keyResource = new ClassPathResource(keyStoreFile);
+            keyStore.load(keyResource.getInputStream(), keyStorePassword.toCharArray());
+            PrivateKey privateKey = (PrivateKey) keyStore.getKey("serialNumber=" + cmmKey + ",CN=" + organizationId, privateKeyPassword.toCharArray());
+
+            // Preparing data string that will be used to derive the signature
+            StringBuilder dataToSign = new StringBuilder();
+            dataToSign.append("cmm-key=").append(cmmKey).append(',');
+            dataToSign.append("content-type=").append(contentType).append(',');
+            dataToSign.append("http-verb=").append(httpVerb).append(',');
+            dataToSign.append("organization-id=").append(organizationId).append(',');
+            dataToSign.append("time=").append(time);
+
+            // Signing data
+            Signature signature = Signature.getInstance("SHA512withRSA", BouncyCastleProvider.PROVIDER_NAME);
+            signature.initSign(privateKey);
+            signature.update(dataToSign.toString().getBytes());
+            byte[] signed = signature.sign();
+            String encoded = ENCODER.encodeToString(signed); // transform byte array to Base64 string
+
+            // populating signature data to HTTP headers
+            headers.set("Authorization", "CWS CWSAccessKeyId:" + encoded);
+            headers.set("HTTP-Verb", httpVerb);
+            headers.set("Organization-Id", organizationId);
+            headers.set("Time", "" + time); // timestamp used to generate signature
+            headers.set("CMM-Key", cmmKey);
+            headers.set("Content-Type", contentType);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public PublicKey decodePublicKey(String derEncodedKey) {
+        try {
+            byte[] keyBytes = DECODER.decode(derEncodedKey);
+            PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(keyBytes));
+            return publicKey;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean verify(final PublicKey publicKey, final String dataToVerify, final String base64Signature) {
+        try {
+            final Signature signInstance = Signature.getInstance("SHA512withRSA");
+            signInstance.initVerify(publicKey);
+            signInstance.update(dataToVerify.getBytes());
+            byte[] signature = DECODER.decode(base64Signature);
+            return signInstance.verify(signature);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
---
+++ java/spring-boot/src/main/java/com/cybersource/flex/models/JsonWebKey.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 CyberSource Corporation. All rights reserved.
+ */
+package com.cybersource.flex.models;
+
+/**
+ * This class conforms to the JSON Web Key (JWK) format as
+ * defined here: https://tools.ietf.org/html/rfc7517#section-4
+ * It's purpose is to allow us to easily serialize out a JWK for
+ * client-side use by the WebCryptoApi.
+ */
+public class JsonWebKey {
+
+    /** Key Type */
+    private String kty;
+    /** Public Key Use */
+    private String use;
+    /** Key ID */
+    private String kid;
+    /** Modulus */
+    private String n;
+    /** Exponent */
+    private String e;
+
+    public String getKty() {
+        return kty;
+    }
+    public void setKty(String keyType) {
+        this.kty = keyType;
+    }
+
+    public String getUse() { return use; }
+    public void setUse(String keyUse) { this.use = keyUse; }
+
+    public String getKid() {
+        return kid;
+    }
+    public void setKid(String keyId) {
+        this.kid = keyId;
+    }
+
+    public String getN() {
+        return n;
+    }
+    public void setN(String modulus) {
+        this.n = modulus;
+    }
+
+    public String getE() { return e; }
+    public void setE(String exponent) {
+        this.e = exponent;
+    }
+}
